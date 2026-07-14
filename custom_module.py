7f2b7bdb6ddbcf8aaac7b19c30120f2ed48f669d
PCalderon123 <pcalderon123@gmail.com>
2026-07-13 20:57:56 -0500

    jupyter files added from home 7-13-26

---
+++ custom_module.py
@@ -0,0 +1,4 @@
+
+# Custom functions to import for time-travel.py
+def generate_time_travel_message(year, destination, cost):
+    return(f"In the year {year}, you will travel to {destination}, and it will cost you ${cost} smakaroos.\n")
